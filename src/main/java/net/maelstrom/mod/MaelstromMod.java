6a4da92893556afbb33b56da1616562d51d360c2
banana-boye <bferraz1313@gmail.com>
2026-02-09 01:33:18 +0000

    Cleanup

---
+++ src/main/java/net/maelstrom/mod/MaelstromMod.java
@@ -0,0 +1,9 @@
+package net.maelstrom.mod;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class MaelstromMod {
+    public static final String MODID = "maelstrom_mod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
